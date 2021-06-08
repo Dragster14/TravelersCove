65f687d91968d29b2dc4cbbb2321172b62f19dd9
Robin Joannes <robinjoannes@gmail.com>
2021-06-08 21:29:01 +0200

    Ajout/modif Pages + section secrète JS

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+// 25ans Marie
+
+function mdp() {
+
+    if (document.getElementById('mdp').value === "Nidalee7") {
+        document.getElementById('article-mdp').classList.add('article_mdp');
+        document.getElementById('article-intro').classList.remove('article_mdp');
+        document.getElementById('article-video').classList.remove('article_mdp');
+        document.getElementById('footer-mdp').classList.remove('bottom-fixe');
+    } else {
+        document.getElementById('mdp').style.border = "solid 2px red";
+        document.getElementById('mdp').placeholder = ":(";
+    }
+}
